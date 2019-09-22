2cbd717b63ef8457eeb41403ff4695e11a305180
Edith Emmings <emmings.edith@gmail.com>
2019-09-22 15:23:49 -0500

    added images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,25 +1,55 @@
 const galleryItems = [
     { 
         id: 1, 
-        path: 'images/goat_small.jpg', 
-        description: 'Photo of a goat taken at Glacier National Park.', 
+        path: 'images/crleton002.jpg', 
+        description: 'Cobwebs on a dry plant', 
         likes: 0 
     }, {
         id: 2,
-        path: 'images/me.png',
-        description: 'Photo of Edith on a dock.',
+        path: 'images/crleton009.jpg',
+        description: 'Little white flowers in front of a fence',
         likes: 0
     }, {
         id: 3,
-        path: 'images/mikcath.png',
-        description: 'Photo of Catherine and Mikayla cheersing.',
+        path: 'images/crleton014.jpg',
+        description: 'Long grass leaning over the pavement',
         likes: 0
     }, {
         id: 4,
-        path: 'images/sam.jpg',
-        description: 'Photo of Sam and Edith on the lightrail.',
+        path: 'images/crleton017.jpg',
+        description: 'Prickley plant in shallow focus',
         likes: 0
-    }, 
+    }, {
+        id: 5,
+        path: 'images/crleton024.jpg',
+        description: 'Daisys in the shadows',
+        likes: 0
+    }, {
+        id: 6,
+        path: 'images/crleton030.jpg',
+        description: 'Weeds in a fallen tree',
+        likes: 0
+    }, {
+        id: 7,
+        path: 'images/crleton070.jpg',
+        description: 'Tall grass shedding its seeds',
+        likes: 0
+    }, {
+        id: 8,
+        path: 'images/crleton076.jpg',
+        description: 'The bedroom on the top floor',
+        likes: 0
+    }, {
+        id: 9,
+        path: 'images/crleton081.jpg',
+        description: 'Piping above the door to the basement',
+        likes: 0
+    }, {
+        id: 10,
+        path: 'images/crleton142.jpg',
+        description: 'Cars parked in front of an electrical substation',
+        likes: 0
+    } 
 ];
 
 module.exports = galleryItems;
